2e3f915c734303e03465b2dd021dfc7d13d517c2
menor <josemenorgmail.com>
2014-03-01 11:14:29 -0800

    Make Simon Says work without calling the server.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,56 @@
-$(document).ready(function(){
-
+$(function() {
+   $('#get_color').on('click', function() {
+    changeRandomCelltoRandomColor()
+   });
 });
+
+function changeRandomCelltoRandomColor() {
+    randomCell().css('background-color', randomColor());
+};
+
+function cellCount() {
+    return $("#color_me li").size();
+};
+
+function randomCell() {
+    randomNumber = Math.floor(Math.random()*cellCount() + 1);
+    return $('#color_me li:nth-child(' + randomNumber + ')' );
+};
+
+function randomColor() {
+    return '#' + randomHexValue();
+};
+
+function randomHexValue() {
+    color = Math.floor(Math.random()*16777215).toString(16);
+    return color;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('#get_color').on('click', function() {
+//         $("#color_me li:nth-child(1)" ).css('background-color', randomHexValue());
+//     })
+    
+// }
+// });
+
+// function Cell() {
+//     this.color = "FFFFFF";
+// }
+
+
+
